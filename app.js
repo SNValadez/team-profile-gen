a114bc5b3a8dd9185ab67434290e06741afdf51b
SNValadez <snvaladez13@gmail.com>
2021-10-15 15:01:46 -0700

    require more /lib resources

--- app.js
+++ app.js
@@ -2,5 +2,13 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+let manager; 
+let teamTitle;
+
 const Employee = require("./lib/Employee1");
 const Engineer = require("./lib/Engineer1");
+const Intern = require("./lib/Intern1");
+const Manager = require("./lib/Manager1");
+
+const teamMates = [];
+
