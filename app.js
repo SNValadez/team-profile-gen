baa919f7a40d3e0149b957a3afe474bf37f23dde
SNValadez <snvaladez13@gmail.com>
2021-10-15 14:57:33 -0700

    require Employee1 and Engineer 1

--- app.js
+++ app.js
@@ -2,4 +2,5 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-const Employee1 = require("./lib/Employee1");
+const Employee = require("./lib/Employee1");
+const Engineer = require("./lib/Engineer1");
